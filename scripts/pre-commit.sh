e69b1e34870bb1034b0f5a0ca846a9844dc9d6bc
yasmine9295 <yasmine.elharim92@gmail.com>
2026-04-09 20:10:36 +0200

    test hook

---
+++ scripts/pre-commit.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo " Vérification du code avant commit..."
+
+# Vérifie si le dossier laravel/laravel existe
+if [ -d "laravel/laravel" ]; then
+  cd laravel/laravel
+
+  # Vérifie si npm est installé
+  if command -v npm >/dev/null 2>&1; then
+    echo "Lancement du linter..."
+    npm run lint
+
+    # Si erreur → bloque commit
+    if [ $? -ne 0 ]; then
+      echo "Linter échoué, commit annulé."
+      exit 1
+    fi
+  else
+    echo "npm non trouvé, lint ignoré"
+  fi
+else
+  echo "Dossier laravel/laravel introuvable"
+fi
+
+echo "Vérification terminée"
+exit 0
