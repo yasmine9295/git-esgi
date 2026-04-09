b44469373b6f4f6049051187a8432d481e01ab96
yasmine9295 <yasmine.elharim92@gmail.com>
2026-04-09 10:56:29 +0200

    liste atelier added, closes #46

---
+++ laravel/resources/views/listeatelier.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Programmes</title>
+</head>
+<body>
+    <h1>Programmes des ateliers</h1>
+
+    <ul>
+        <li>Atelier 1</li>
+        <li>Atelier 2</li>
+        <li>Atelier 3</li>
+    </ul>
+
+    <a href="/clients/connexion">Se connecter</a>
+</body>
+</html>
