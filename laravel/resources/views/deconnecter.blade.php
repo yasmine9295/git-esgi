aafa089b8cbeca9bd698b1e8e9ef6d95ce1ed26d
yasmine9295 <yasmine.elharim92@gmail.com>
2026-04-09 14:47:30 +0200

    views added, closes #55

---
+++ laravel/resources/views/deconnecter.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title> SBateliers </title>
+    </head>
+    <body>
+        <h2>Vous etes deconnectés</h2>
+    </body> 
+</html>
---
+++ laravel/resources/views/espace.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title> SBateliers </title>
+    </head>
+    <body>
+        <h2>Bienvenue  !!!!!!!!</h2>
+    </body> 
+</html>
